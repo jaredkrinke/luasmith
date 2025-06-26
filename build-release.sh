9f4ee691647c3ed045a9d6c88d0f2755e4b28a80
Jared Krinke <jared.krinke@gmail.com>
2025-06-26 13:12:34 -0700

    Add APE/cosmocc build

--- build-release.sh
+++ build-release.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+# Statically-linked Linux build, using zig cc
 make clean
 make CC="zig cc --target=x86_64-linux-musl"
 strip luasmith
-tar czf luasmith-$1-linux-x86_64.tar.gz --transform "s,^,luasmith-$1/," luasmith
+tar czf luasmith-$1-linux-x86_64.tar.gz luasmith
+
+# Multi-platform build, using cosmocc
+make clean
+make CC=cosmocc MYCFLAGS="-Os -s"
+cp luasmith luasmith.com
+zip luasmith-$1-universal.zip luasmith.com
 
