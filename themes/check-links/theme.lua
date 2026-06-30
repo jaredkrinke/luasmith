d67ec8d0068c865ffd77d504f54cbd6380dd51a2
scms <scms@mail.schemescape.com>
2026-06-29 22:44:15 -0700

    Fix check-links utility

--- themes/check-links/theme.lua
+++ themes/check-links/theme.lua
@@ -3,7 +3,7 @@ local source = args[3] or "."
 return {
 	readFromSource(source),
 	checkLinks(),
-	createProcessingNode(function (changes)
+	processItems(function (changes)
 		print("\nLink checking completed. See warnings above for any broken links.\n")
 	end)
 }
